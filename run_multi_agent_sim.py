10a9f6b32deee7cee60d1d1d3a8fc1a5f448dd1b
joris997 <joris.verhagen@gmail.com>
2024-05-23 11:16:00 +0200

    fixed plot_movements

--- run_multi_agent_sim.py
+++ run_multi_agent_sim.py
@@ -58,7 +58,7 @@ if __name__ == "__main__":
     ####################
     # Time-lapse plots #
     ####################
-    sim = main(world, plot=False)
+    sim = main(world, plot=True)
 
     fig, axs = plt.subplots(2,5, figsize=(15,6))
     times = np.linspace(0, len(sim.history)-1, 10).astype(int)
@@ -78,136 +78,136 @@ if __name__ == "__main__":
         ax.set_yticks([])
     plt.savefig("figures/time_lapse.png", dpi=300)
 
-    ###########################
-    # Run several simulations #
-    ###########################
-    sims = []
-    # create tqdm
-    N = 100
-    pbar = tqdm(total=N)
-    for i in range(N):
-        sims.append(main(world))
-        pbar.update(1)
-    pbar.close()
+    # ###########################
+    # # Run several simulations #
+    # ###########################
+    # sims = []
+    # # create tqdm
+    # N = 100
+    # pbar = tqdm(total=N)
+    # for i in range(N):
+    #     sims.append(main(world))
+    #     pbar.update(1)
+    # pbar.close()
 
-    ###############
-    # Density Fig #
-    ###############
-    mean_pos_teams = []
-    for sim in sims:
-        # compute the mean position of each team
-        mean_pos_teams.append(get_density(sim,rotate=False))
-    mean_pos_teams = np.array(mean_pos_teams)
+    # ###############
+    # # Density Fig #
+    # ###############
+    # mean_pos_teams = []
+    # for sim in sims:
+    #     # compute the mean position of each team
+    #     mean_pos_teams.append(get_density(sim,rotate=False))
+    # mean_pos_teams = np.array(mean_pos_teams)
 
-    # Create a heatmap of the mean position of each team
-    # Create a grid of worldsize and add an occurrence of 1 at each position of the mean position of each team
-    grid_world = World([-100,100])
-    density = np.zeros((int(grid_world.x_lim[-1]-grid_world.x_lim[0]), 
-                        int(grid_world.y_lim[-1]-grid_world.y_lim[0])))
+    # # Create a heatmap of the mean position of each team
+    # # Create a grid of worldsize and add an occurrence of 1 at each position of the mean position of each team
+    # grid_world = World([-100,100])
+    # density = np.zeros((int(grid_world.x_lim[-1]-grid_world.x_lim[0]), 
+    #                     int(grid_world.y_lim[-1]-grid_world.y_lim[0])))
     
-    for it,data in enumerate(mean_pos_teams):
-        # team A -> +1
-        x,y = pos_to_grid(data[0], density, grid_world)
-        xs,ys = circle_around_index(x,y, 5, density)
-        for x,y in zip(xs,ys):
-            density[x,y] += 1
-        # team B -> -1
-        x,y = pos_to_grid(data[1], density, grid_world)
-        xs,ys = circle_around_index(x,y, 5, density)
-        for x,y in zip(xs,ys):
-            density[x,y] -= 1
+    # for it,data in enumerate(mean_pos_teams):
+    #     # team A -> +1
+    #     x,y = pos_to_grid(data[0], density, grid_world)
+    #     xs,ys = circle_around_index(x,y, 5, density)
+    #     for x,y in zip(xs,ys):
+    #         density[x,y] += 1
+    #     # team B -> -1
+    #     x,y = pos_to_grid(data[1], density, grid_world)
+    #     xs,ys = circle_around_index(x,y, 5, density)
+    #     for x,y in zip(xs,ys):
+    #         density[x,y] -= 1
 
-    # Plot the heatmap
-    fig, ax = plt.subplots()
-    ax.imshow(density.T, cmap='coolwarm', interpolation='hermite',
-              extent=[grid_world.x_lim[0], grid_world.x_lim[1], grid_world.y_lim[0], grid_world.y_lim[1]])
-    # change the x and y ticks to match grid_world
-    x_range = np.linspace(grid_world.x_lim[0], grid_world.x_lim[1], density.shape[0])
-    y_range = np.linspace(grid_world.y_lim[0], grid_world.y_lim[1], density.shape[1])
-    ax.set_xticks(np.arange(grid_world.x_lim[0], grid_world.x_lim[1]+0.1, 20))
-    ax.set_yticks(np.arange(grid_world.y_lim[0], grid_world.y_lim[1]+0.1, 20))
-    plt.savefig("figures/density.png", dpi=300)
+    # # Plot the heatmap
+    # fig, ax = plt.subplots()
+    # ax.imshow(density.T, cmap='coolwarm', interpolation='hermite',
+    #           extent=[grid_world.x_lim[0], grid_world.x_lim[1], grid_world.y_lim[0], grid_world.y_lim[1]])
+    # # change the x and y ticks to match grid_world
+    # x_range = np.linspace(grid_world.x_lim[0], grid_world.x_lim[1], density.shape[0])
+    # y_range = np.linspace(grid_world.y_lim[0], grid_world.y_lim[1], density.shape[1])
+    # ax.set_xticks(np.arange(grid_world.x_lim[0], grid_world.x_lim[1]+0.1, 20))
+    # ax.set_yticks(np.arange(grid_world.y_lim[0], grid_world.y_lim[1]+0.1, 20))
+    # plt.savefig("figures/density.png", dpi=300)
 
-    #####################
-    # Rotated Histogram #
-    #####################
-    # here we get the mean density of both teams, and rotate the mean position of team A to the x-axis
-    # as the agents always converge to a line, we can plot this then on a 1D histogram
-    mean_pos_teams = []
-    for sim in sims:
-        # compute the mean position of each team
-        mean_pos_teams.append(get_density(sim,rotate=True))
-    mean_pos_teams = np.array(mean_pos_teams)
+    # #####################
+    # # Rotated Histogram #
+    # #####################
+    # # here we get the mean density of both teams, and rotate the mean position of team A to the x-axis
+    # # as the agents always converge to a line, we can plot this then on a 1D histogram
+    # mean_pos_teams = []
+    # for sim in sims:
+    #     # compute the mean position of each team
+    #     mean_pos_teams.append(get_density(sim,rotate=True))
+    # mean_pos_teams = np.array(mean_pos_teams)
 
-    # indexing is [simulation, team, x/y]
-    mean_x_pos_teams = mean_pos_teams[:,:,0]
-    mean_x_pos_A = mean_x_pos_teams[:,0]
-    mean_x_pos_B = mean_x_pos_teams[:,1]
+    # # indexing is [simulation, team, x/y]
+    # mean_x_pos_teams = mean_pos_teams[:,:,0]
+    # mean_x_pos_A = mean_x_pos_teams[:,0]
+    # mean_x_pos_B = mean_x_pos_teams[:,1]
 
-    # Create a histogram of the mean position of each team
-    fig, axs = plt.subplots(1,1, figsize=(12,6))
-    axs.hist(mean_x_pos_A, bins=20, alpha=0.5, color='blue', label='Team A')
-    axs.hist(mean_x_pos_B, bins=20, alpha=0.5, color='red', label='Team B')
-    axs.set_xlabel("Mean x position")
-    axs.set_ylabel("Frequency")
-    axs.legend()
-    plt.savefig("figures/histogram.png", dpi=300)
+    # # Create a histogram of the mean position of each team
+    # fig, axs = plt.subplots(1,1, figsize=(12,6))
+    # axs.hist(mean_x_pos_A, bins=20, alpha=0.5, color='blue', label='Team A')
+    # axs.hist(mean_x_pos_B, bins=20, alpha=0.5, color='red', label='Team B')
+    # axs.set_xlabel("Mean x position")
+    # axs.set_ylabel("Frequency")
+    # axs.legend()
+    # plt.savefig("figures/histogram.png", dpi=300)
 
-    ####################
-    # Seperation index #
-    ####################
-    # here we plot the separation index of each simulation
-    # orderliness = avg_inter_team_distance / (avg_intra_team_distance_A + avg_intra_team_distance_B)/2
-    # Get the separation of all sims over time
-    orderlinesses = []
-    intra_team_distances = []
-    for sim in sims:
-        indices = []
-        distances = []
-        # we do this for each sim, loop through the history and get the separation index
-        for sim_state in sim.history:
-            orderliness, total_intra_team_distance = get_separation_index(sim_state)
-            indices.append(orderliness)
-            distances.append(total_intra_team_distance)
-        orderlinesses.append(indices)
-        intra_team_distances.append(distances)
+    # ####################
+    # # Seperation index #
+    # ####################
+    # # here we plot the separation index of each simulation
+    # # orderliness = avg_inter_team_distance / (avg_intra_team_distance_A + avg_intra_team_distance_B)/2
+    # # Get the separation of all sims over time
+    # orderlinesses = []
+    # intra_team_distances = []
+    # for sim in sims:
+    #     indices = []
+    #     distances = []
+    #     # we do this for each sim, loop through the history and get the separation index
+    #     for sim_state in sim.history:
+    #         orderliness, total_intra_team_distance = get_separation_index(sim_state)
+    #         indices.append(orderliness)
+    #         distances.append(total_intra_team_distance)
+    #     orderlinesses.append(indices)
+    #     intra_team_distances.append(distances)
     
-    # Now plot the orderliness over time
-    fig, axs = plt.subplots(1,1, figsize=(12,6))
-    for sim in orderlinesses:
-        axs.plot(sim)
-    axs.set_xlabel("Time")
-    axs.set_ylabel("Orderliness index")
-    plt.savefig("figures/orderliness_index.png", dpi=300)
+    # # Now plot the orderliness over time
+    # fig, axs = plt.subplots(1,1, figsize=(12,6))
+    # for sim in orderlinesses:
+    #     axs.plot(sim)
+    # axs.set_xlabel("Time")
+    # axs.set_ylabel("Orderliness index")
+    # plt.savefig("figures/orderliness_index.png", dpi=300)
 
-    # Now plot the intra team distances over time
-    fig, axs = plt.subplots(1,1, figsize=(12,6))
-    for sim in intra_team_distances:
-        axs.plot(sim)
-    axs.set_xlabel("Time")
-    axs.set_ylabel("Intra team distances")
-    plt.savefig("figures/intra_team_distances_index.png", dpi=300)
+    # # Now plot the intra team distances over time
+    # fig, axs = plt.subplots(1,1, figsize=(12,6))
+    # for sim in intra_team_distances:
+    #     axs.plot(sim)
+    # axs.set_xlabel("Time")
+    # axs.set_ylabel("Intra team distances")
+    # plt.savefig("figures/intra_team_distances_index.png", dpi=300)
 
-    # Create a plot with the mean and standard deviation over time for all the different simulations
-    mean_orderlinesses_index = np.mean(orderlinesses, axis=0)
-    std_orderlinesses_index = np.std(orderlinesses, axis=0)
+    # # Create a plot with the mean and standard deviation over time for all the different simulations
+    # mean_orderlinesses_index = np.mean(orderlinesses, axis=0)
+    # std_orderlinesses_index = np.std(orderlinesses, axis=0)
 
-    mean_intra_team_distances_index = np.mean(intra_team_distances, axis=0)
-    std_intra_team_distances_index = np.std(intra_team_distances, axis=0)
+    # mean_intra_team_distances_index = np.mean(intra_team_distances, axis=0)
+    # std_intra_team_distances_index = np.std(intra_team_distances, axis=0)
 
-    fig, axs = plt.subplots(1,2, figsize=(12,6))
-    axs[0].plot(mean_orderlinesses_index)
-    axs[0].fill_between(range(len(mean_orderlinesses_index)), 
-                     mean_orderlinesses_index-std_orderlinesses_index, 
-                     mean_orderlinesses_index+std_orderlinesses_index, alpha=0.5)
-    axs[0].set_xlabel("Time")
-    axs[0].set_ylabel("Separation index")
+    # fig, axs = plt.subplots(1,2, figsize=(12,6))
+    # axs[0].plot(mean_orderlinesses_index)
+    # axs[0].fill_between(range(len(mean_orderlinesses_index)), 
+    #                  mean_orderlinesses_index-std_orderlinesses_index, 
+    #                  mean_orderlinesses_index+std_orderlinesses_index, alpha=0.5)
+    # axs[0].set_xlabel("Time")
+    # axs[0].set_ylabel("Separation index")
 
-    axs[1].plot(mean_intra_team_distances_index)
-    axs[1].fill_between(range(len(mean_intra_team_distances_index)), 
-                     mean_intra_team_distances_index-std_intra_team_distances_index, 
-                     mean_intra_team_distances_index+std_intra_team_distances_index, alpha=0.5)
-    axs[1].set_xlabel("Time")
-    axs[1].set_ylabel("Intra team distances")
+    # axs[1].plot(mean_intra_team_distances_index)
+    # axs[1].fill_between(range(len(mean_intra_team_distances_index)), 
+    #                  mean_intra_team_distances_index-std_intra_team_distances_index, 
+    #                  mean_intra_team_distances_index+std_intra_team_distances_index, alpha=0.5)
+    # axs[1].set_xlabel("Time")
+    # axs[1].set_ylabel("Intra team distances")
 
-    plt.savefig("figures/indices_mean_std.png", dpi=300)
+    # plt.savefig("figures/indices_mean_std.png", dpi=300)
--- src/plot_agents.py
+++ src/plot_agents.py
@@ -3,40 +3,44 @@ from matplotlib import pyplot as plt
 from matplotlib.animation import FuncAnimation
 from matplotlib.patches import Circle, Patch
 
-def animate_agents(i, agent_circles, positions):
+def animate_agents(i, agent_circles, steps):
     """Update the position of each agent's circles (both position and sensing radius)."""
     updated_artists = []
-    for agent_data, position in zip(agent_circles, positions[i]):
+    for agent_data, agent in zip(agent_circles, steps[i]):
         position_circle, sensing_circle = agent_data
-        position_circle.center = position
-        sensing_circle.center = position
+        position_circle.center = agent.position
+        sensing_circle.center = agent.position
         updated_artists.extend([position_circle, sensing_circle])
     return updated_artists
 
 def plot_movements(sim, anim_length=10, anim_fps=12):
     """Animate the movements of all agents, including their sensing radius."""
-
     skip_frames = max(int(len(sim.history)/(anim_length * anim_fps)), 1)
-    positions = np.array(sim.history[::skip_frames])  # Assuming sim.history is a list of positions for each step
+    steps = np.array(sim.history[::skip_frames])  # Assuming sim.history is a list of positions for each step
     
     fig, ax = plt.subplots()
     ax.set_xlim(0, sim.world.world_size)
     ax.set_ylim(0, sim.world.world_size)
 
-    colors = ['red' if agent.agent_type == 'A' else 'blue' if agent.agent_type == 'B' else 'green' for agent in sim.agents]
     agent_circles = []  # This will store tuples of (position_circle, sensing_circle) for each agent
     
-    for pos, color, agent in zip(positions[0], colors, sim.agents):
-        position_circle = Circle(pos, radius=agent.radius, color=color)
+    for agent in steps[0]:
+        if agent.agent_type == 'A':
+            color = 'red'
+        elif agent.agent_type == 'B':
+            color = 'blue'
+        else:
+            color = 'green'
+        position_circle = Circle(agent.position, radius=agent.radius, color=color)
         ax.add_patch(position_circle)
         
         # We can always add the sensing circle because radius=0 if we don't care about it (and you don't see it)
-        sensing_circle = Circle(pos, radius=agent.sensing_radius, color=color, fill=False, linestyle='--')
+        sensing_circle = Circle(agent.position, radius=agent.sensing_radius, color=color, fill=False, linestyle='--')
         ax.add_patch(sensing_circle)
         
         agent_circles.append((position_circle, sensing_circle))
     
-    anim = FuncAnimation(fig, animate_agents, frames=len(positions), interval=1e3/anim_fps, fargs=(agent_circles, positions), blit=True)
+    anim = FuncAnimation(fig, animate_agents, frames=len(steps), interval=1e3/anim_fps, fargs=(agent_circles, steps), blit=True)
     
     ax.set_aspect('equal')
 
